20b1fbbbf7f19c2a08bc424fb3c724e94c1a156e
Mr-Mason <hello_mason@163.com>
2019-05-06 22:07:06 +0800

    RxJava操作符

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.rxjava
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.bean1.Person
+import android.widget.Toast
 import com.example.bean1.PersonRespon
+import com.example.bean2.Person
 import com.example.bean2.Plan
 import io.reactivex.Observable
+import io.reactivex.ObservableSource
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
@@ -20,13 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_map.setOnClickListener {
-
-
             flatMap()
         }
     }
 
-    private fun map2() {
+    private fun map() {
 
         Observable.just(1, 2, 3, 4, 5)
             .map { int -> "$int" }
@@ -49,57 +49,14 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
-    private fun map() {
-        var person1 = Person("张三", 25, "say hello")
-        var person2 = Person("李四", 25, "say hello")
-        var listPerson = ArrayList<Person>()
-        listPerson.add(person1)
-        listPerson.add(person2)
-        Observable.just(listPerson).map(object : Function<List<Person>, PersonRespon> {
-            override fun apply(t: List<Person>): PersonRespon {
-                if (t.isNotEmpty()) {
-                    for (person in t) {
-                        Log.d("name", "${person.name}")
-                        return PersonRespon(person.name)
-                    }
-                    t.forEach { person -> Log.d("person.name:", "${person.name}") }
-                }
-                return PersonRespon("")
+    private fun flatMap() {
+        val nameList = listOf<String>("张三", "李四", "王五")
+        Observable.just(nameList)
+            .flatMap { list ->
+                Observable.fromIterable(list)
             }
-        })
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<PersonRespon> {
-                override fun onComplete() {
-
-                }
-
-                override fun onSubscribe(d: Disposable) {
-
-                }
-
-                override fun onNext(t: PersonRespon) {
-                    Log.d("onNext", "${t.id}")
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.d("onError", "$e")
-                }
-            })
-
-    }
-
-    private fun flatMap() {
-        var personList = ArrayList<com.example.bean2.Person>()
-        val planList1 = ArrayList<Plan>()
-        val planList2 = ArrayList<Plan>()
-        planList1.add(Plan("张三", "我要去爬山"))
-        planList2.add(Plan("李四", "我要去滑雪"))
-        personList.add(com.example.bean2.Person("张三", planList1))
-        personList.add(com.example.bean2.Person("李四", planList2))
-        Observable.fromIterable(personList)
-            .flatMap { personList -> Observable.fromIterable(personList.planList) }
-            .flatMap { planList -> Observable.fromIterable(planList.actionList) }
             .subscribe(object : Observer<String> {
                 override fun onComplete() {
 
@@ -110,14 +67,12 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onNext(t: String) {
-                    Log.d("onNext", "$t")
+                    Toast.makeText(this@MainActivity, "$t", Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onError(e: Throwable) {
 
                 }
             })
-
-
     }
 }
